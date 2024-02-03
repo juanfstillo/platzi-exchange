cf4edc751dcf0e09cbdace8cba89497faf9c68be
juan.stillo <juan.stillo@trenesargentinos.gob.ar>
2024-02-03 12:01:40 -0300

    pre deploy

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -52,22 +52,27 @@
 
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
+            @click="toggleConverter"
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
           >
-            Cambiar
+            {{ fromUsd ? `USD A ${asset.symbol}` : `${asset.symbol} a USD` }}
           </button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
               <input
+                v-model="convertValue"
                 id="convertValue"
                 type="number"
                 class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                :placeholder="`Valor en ${fromUsd ? 'USD' : asset.symbol}`"
               />
             </label>
           </div>
 
-          <span class="text-xl"></span>
+          <span class="text-xl">
+            {{ convertResult }} {{ fromUsd ? asset.symbol : `USD` }}
+          </span>
         </div>
       </div>
       <line-chart
@@ -122,6 +127,8 @@ export default {
       history: [],
       isLoading: false,
       markets: [],
+      fromUsd: true,
+      convertValue: null,
     };
   },
   created() {
@@ -129,6 +136,9 @@ export default {
   },
 
   methods: {
+    toggleConverter() {
+      this.fromUsd = !this.fromUsd;
+    },
     getCoin() {
       const id = this.$route.params.id;
       this.isLoading = true;
@@ -157,6 +167,17 @@ export default {
     },
   },
   computed: {
+    convertResult() {
+      if (!this.convertValue) {
+        return 0;
+      }
+
+      const result = this.fromUsd
+        ? this.convertValue / this.asset.priceUsd
+        : this.convertValue * this.asset.priceUsd;
+
+      return result.toFixed(4);
+    },
     min() {
       return Math.min(
         ...this.history.map((h) => parseFloat(h.priceUsd).toFixed(2))
@@ -175,5 +196,16 @@ export default {
       );
     },
   },
+  watch: {
+    $route() {
+      this.getCoin();
+    },
+  },
 };
 </script>
+<style scoped>
+td {
+  padding: 10px;
+  text-align: center;
+}
+</style>
